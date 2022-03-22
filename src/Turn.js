36a1b7e84e5c259541221f47ff89ead8d9702ae7
Angele Williams <angele.williams5@gmail.com>
2022-03-21 23:16:02 -0600

    Write implementation code to pass Turn method tests

--- src/Turn.js
+++ src/Turn.js
@@ -5,12 +5,37 @@ class Turn {
   constructor(userGuess, card){
     this.userGuess = userGuess;
     this.currentCard = card;
+    this.guessedCorrectly;
+  }
+  returnGuess(){
+    return this.userGuess;
+  }
+  returnCard(){
+    return this.currentCard;
+  }
+  evaluateGuess(){
+    //maybe refactor to switch statement later
+    if(this.userGuess === this.currentCard.correctAnswer){
+      this.guessedCorrectly = true;
+    }
+    else { this.guessedCorrectly = false;}
+    return this.guessedCorrectly;
+  }
+  giveFeedback(){
+    //maybe refactor to switch statement later
+    if(this.guessedCorrectly){
+      return 'correct!'
+    }
+    return 'incorrect!'
   }
 }
 
 module.exports =
  Turn;
 
+
+//Turn Class Notes
+//Instantiated with two arguments - a string (that represents a user’s guess to the question), and a Card object for the current card in play.
 
  // returnGuess: method that returns the guess
  // returnCard: method that returns the Card
