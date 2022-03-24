303870f480c388d1d3cc445783ed13abd3b7b521
Angele Williams <angele.williams5@gmail.com>
2022-03-23 22:58:32 -0600

    Complete Deck and Round tests and implementation code

--- test/Round-test.js
+++ test/Round-test.js
@@ -6,62 +6,88 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
-describe('Round',  function(){
+describe('Round', () => {
+  let card1, card2, card3, deck, round;
+  beforeEach(() => {
+     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+     card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+     deck = new Deck([card1, card2, card3]);
+     round = new Round(deck);
+  });
 
-  it('should be a function', function (){
+  it('should be a function', () => {
     const round = new Round();
     expect(Round).to.be.a('function');
   });
 
-  it('should be instantiate a round', function (){
+  it('should be instantiate a round', () => {
     const round = new Round();
     expect(round).to.be.an.instanceOf(Round);
   });
 
-  it('should accept a deck as the input', function(){
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck)
+  it('should accept a deck as the input', () => {
+
     expect(round.deck).to.equal(deck);
   });
 
-  it('returnCurrentCard should return the current card being played', function(){
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck)
-    expect(round.returnCurrentCard()).to.equal(card1);
+  it('returnCurrentCard should return the current card being played', () => {
+    expect(round.returnCurrentCard(round.turns)).to.equal(card1);
+    round.takeTurn('pug');
+    expect(round.returnCurrentCard(round.turns)).to.equal(card2);
   });
 
-  it.skip('takeTurn should update turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses', function(){
-    // When a guess is made, a new Turn instance is created.
-
+  it('takeTurn should update turns count', () => {
+    expect(round.turns).to.equal(0);
+    round.takeTurn();
+    expect(round.turns).to.equal(1);
   });
 
-  it.skip('should', function(){});
-  it.skip('should', function(){});
-  it.skip('should', function(){});
+  it('takeTurn should evaluate guesses and give feedback', () => {
+    expect(round.takeTurn('sea otter')).to.equal('correct!');
+  });
 
+  it('takeTurn should store ids of incorrect guesses in an array', () => {
+
+    round.takeTurn('sea otter');
+    expect(round.incorrectGuesses).to.deep.equal([]);
+    round.takeTurn('spleen');
+    expect(round.incorrectGuesses).to.deep.equal([14]);
+  });
+
+  it('calculatePercentCorrect should calculate what number of guesses were correct', () => {
+    round.takeTurn('sea otter');
+    round.takeTurn('spleen');
+    round.takeTurn('Lex');
+    expect(round.calculatePercentCorrect()).to.equal(34)
+  });
+  ///something like round.incorrectGuesses.length divided by turns
+  it('something should do something', () => {
+    round.takeTurn('sea otter');
+    round.takeTurn('spleen');
+    expect(round.endRound()).to.equal('** Round over! ** You answered 50% of the questions correctly!')
+  });
+
+  it.skip('something should do something', () => {});
+
+  it.skip('', () =>{});
 
 });
 //notes pasted from project spec
 //All of these need to be expect statements
-
-round.returnCurrentCard(); // => { first card1 }
-
-round.turns; // => 0
-round.turns; // => 2
-// turns count should increase after take turn
-round.incorrectGuesses;     // => []
-
-round.takeTurn('sea otter'); // => 'correct!'
-round.takeTurn('spleen');   // => 'incorrect!'
-//take turn should update turns
-
-round.incorrectGuesses;     // => [14]
-
-round.returnCurrentCard(); // => { id: 12,}
-round.calculatePercentCorrect(); // => 50
+//
+// round.returnCurrentCard(); // => { first card1 }
+//
+// round.turns; // => 0
+// round.turns; // => 2
+// // turns count should increase after take turn
+// round.incorrectGuesses;     // => []
+//
+// round.takeTurn('sea otter'); // => 'correct!'
+// round.takeTurn('spleen');   // => 'incorrect!'
+// //take turn should update turns
+//
+// round.incorrectGuesses;     // => [14]
+//
+// round.returnCurrentCard(); // => { id: 12,}
+// round.calculatePercentCorrect(); // => 50
